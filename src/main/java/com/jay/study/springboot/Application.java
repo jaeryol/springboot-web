2278e489e63d5ce3ea9d35869cb67f681b4ed8b7
jaeryongim <hackerrs.im@gmail.com>
2020-05-17 14:53:30 +0900

    HelloController를 추가한다
    hello 문자열을 반환하는 API를 추가한다.
    또한 이를 검증하기 위한 단위 테스트를 추가한다.

---
+++ src/main/java/com/jay/study/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.jay.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        // 내장 AWS 실행
+        SpringApplication.run(Application.class, args);
+    }
+}
