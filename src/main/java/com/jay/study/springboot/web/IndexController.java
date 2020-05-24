09e120b85697ead0e2082a6014205a7aa71745f7
jaeryongim <hackerrs.im@gmail.com>
2020-05-24 23:29:06 +0900

    세션 유지를 위한 어노테이션 추가 및 적용
    세션에서 로그인 정보를 가져오는 코드는 계속 반복될 수 있어
    어노테이션을 만들어 간단하게 적용할 수 있도록 한다.

--- src/main/java/com/jay/study/springboot/web/IndexController.java
+++ src/main/java/com/jay/study/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jay.study.springboot.web;
 
+import com.jay.study.springboot.config.auth.LoginUser;
 import com.jay.study.springboot.config.auth.dto.SessionUser;
 import com.jay.study.springboot.service.posts.PostsService;
 import com.jay.study.springboot.web.dto.PostsResponseDto;
@@ -15,12 +16,11 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class IndexController {
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model,
+                        @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
