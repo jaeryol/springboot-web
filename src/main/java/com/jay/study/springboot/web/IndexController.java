70998ba3e2efe84e82688aa4a23f45e2834a4b8c
jaeryongim <hackerrs.im@gmail.com>
2020-05-24 22:52:59 +0900

    구글 로그인 기능 추가하기
    구글 로그인에 필요한 기능들을 추가한다.

--- src/main/java/com/jay/study/springboot/web/IndexController.java
+++ src/main/java/com/jay/study/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jay.study.springboot.web;
 
+import com.jay.study.springboot.config.auth.dto.SessionUser;
 import com.jay.study.springboot.service.posts.PostsService;
 import com.jay.study.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +9,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
